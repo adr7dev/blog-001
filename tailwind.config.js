674923063321bd9d62eda92c293d6d543fb9d1ba
Adrian Parniewicz <adr7dev@gmail.com>
2021-10-07 09:56:09 +0200

    NextJS installation with basic setup

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false,
+}
